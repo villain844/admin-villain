a6bc34b563e320be60e04b7c116934abcce3e090
villain844 <samyonosamyoni@gmail.com>
2025-07-03 20:26:41 +0700

    Update pesan.js

--- api/pesan.js
+++ api/pesan.js
@@ -1,9 +1,9 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch("https://sheetdb.io/api/v1/5ni7a9sbf13p3");
+    const response = await fetch('https://sheetdb.io/api/v1/hkydnwssgudey');
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: "Gagal mengambil data" });
+    res.status(500).json({ error: 'Gagal mengambil data dari SheetDB' });
   }
 }
