5c5ca4eee9986a56169a14473e91ee5acc261de0
villain844 <samyonosamyoni@gmail.com>
2025-07-04 21:14:27 +0700

    Delete api directory

--- api/pesan.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  try {
-    const response = await fetch('https://sheetdb.io/api/v1/hkydnwssgudey');
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: 'Gagal mengambil data dari SheetDB' });
-  }
-}
